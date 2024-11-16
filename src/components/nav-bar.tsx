1bb6ab0ba3e5eab7e77cba46287c9100d2c87d1d
Usman Hassan <usmanhassangu@gmail.com>
2024-11-16 15:48:51 +0100

    Add Sheet component for improved UI; integrate into NavBar for better mobile navigation

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -8,6 +8,8 @@ import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { MenuIcon } from "lucide-react";
 
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
 const NavBar = () => {
   const pathname = usePathname();
   return (
@@ -30,9 +32,33 @@ const NavBar = () => {
       <Button size={"sm"} className="hidden sm:block">
         Join the Community
       </Button>
-      <Button size={"icon-sm"} variant={"ghost"} className="md:hidden">
-        <MenuIcon size={24} />
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size={"icon-sm"} variant={"ghost"} className="md:hidden">
+            <MenuIcon size={24} />
+          </Button>
+        </SheetTrigger>
+        <SheetContent>
+          <Link href={"/"}>
+            <LogoLarge />
+          </Link>
+          <ul className="flex flex-col  gap-5 my-5">
+            {links.map((link) => (
+              <li
+                key={link.name}
+                className={cn({
+                  "text-primary": pathname.startsWith(link.href)
+                })}
+              >
+                <Link href={link.href}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+          <Button size={"default"} className="w-full">
+            Join the Community
+          </Button>
+        </SheetContent>
+      </Sheet>
     </nav>
   );
 };
