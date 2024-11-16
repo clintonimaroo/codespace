7d588a2a80d3b5a2ee298a3102d8c3224b61c78a
Usman Hassan <usmanhassangu@gmail.com>
2024-11-16 09:35:19 +0100

    Refactor JoinSection layout for improved responsiveness; adjust text sizes and aspect ratios

--- src/components/join-section.tsx
+++ src/components/join-section.tsx
@@ -4,16 +4,16 @@ import { JoinUsImageCards } from "./image-cards";
 
 const JoinSection = () => {
   return (
-    <section className="container grid grid-cols-2 py-20">
+    <section className="container grid grid-cols-1 md:grid-cols-2 py-20">
       <div className="space-y-5 flex flex-col ">
         <h3 className="text-xl text-primary font-medium">JOIN CODE SPACE!!</h3>
-        <p className="text-gray-800 text-3xl">
+        <p className="text-gray-800 text-2xl sm:text-3xl">
           Be a part of our ever growing community by joining our Telegram
           channel and getting periodic updates.
         </p>
         <Button className="w-fit">Join the community</Button>
       </div>
-      <div className="w-full aspect-[16/10]">
+      <div className="w-full aspect-square md:aspect-[16/10] md:mt-0 mt-20">
         <JoinUsImageCards />
       </div>
     </section>
