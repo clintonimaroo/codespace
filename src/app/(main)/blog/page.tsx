b03a03d47b8a2bec267496f29c2d47fe00d3d132
namecannotbeblankk <93707197+clintonimaroo@users.noreply.github.com>
2025-01-11 03:37:30 +0100

    fixx

--- src/app/(main)/blog/page.tsx
+++ src/app/(main)/blog/page.tsx
@@ -4,30 +4,34 @@ import JoinSection from "@/components/join-section";
 import { Logo } from "@/components/brand";
 import Container from "@/components/container";
 import Blog from "@/components/Blog/Index";
+import { Suspense } from 'react';
+
 
 export default function BlogPage() {
   return (
     <>
       {/* Hero Section */}
-      <section className="container gap-5 py-5 md:py-20 ">
-        <div className="space-y-5 flex-col flex justify-center items-center text-center sm:w-1/2 mx-auto">
-          <Logo className="scale-75" />
-          <SpaceBadge>CODE SPACE BLOG</SpaceBadge>
-          <h1 className="font-medium text-3xl md:text-5xl leading-snug">
-            The Code Space Blog
-          </h1>
-          <p className="text-xl subtitle max-w-xl">
-            Stay updated with the latest news, Insights, stories, and updates from the heart of our community.
-          </p>
-        </div>
-      </section>
+      <Suspense fallback={<div>Loading...</div>}>
+        <section className="container gap-5 py-5 md:py-20 ">
+          <div className="space-y-5 flex-col flex justify-center items-center text-center sm:w-1/2 mx-auto">
+            <Logo className="scale-75" />
+            <SpaceBadge>CODE SPACE BLOG</SpaceBadge>
+            <h1 className="font-medium text-3xl md:text-5xl leading-snug">
+              The Code Space Blog
+            </h1>
+            <p className="text-xl subtitle max-w-xl">
+              Stay updated with the latest news, Insights, stories, and updates from the heart of our community.
+            </p>
+          </div>
+        </section>
 
-      <Container className="container space-y-2 pb-10">
-        <Blog />
-      </Container>
+        <Container className="container space-y-2 pb-10">
+          <Blog />
+        </Container>
 
-      <BrandsSection />
-      <JoinSection />
+        <BrandsSection />
+        <JoinSection />
+      </Suspense>
     </>
   );
 }
