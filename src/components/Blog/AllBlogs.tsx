2943d9fb45cde22efd18b2187b2fc83331275f97
Clinton Imaro <93707197+clintonimaroo@users.noreply.github.com>
2025-01-11 01:11:14 +0100

    Update AllBlogs.tsx

--- src/components/Blog/AllBlogs.tsx
+++ src/components/Blog/AllBlogs.tsx
@@ -18,8 +18,8 @@ export default function AllBlogs({
               <Image
                 className="h-64 bg-[#f8f8f8] rounded-[10px] w-full object-cover"
                 src={blog.featuredImage.url}
-                width={blog.featuredImage.width}
-                height={blog.featuredImage.height}
+                width={500}  // Add default width
+                height={400} // Add default height
                 alt={blog.featuredImage.alt || blog.title}
               />
             )}
