558d0518aca429edb1d4df24985682377d9cbaf8
namecannotbeblankk <93707197+clintonimaroo@users.noreply.github.com>
2025-01-11 22:17:00 +0100

    change

--- src/app/(main)/blog/page.tsx
+++ src/app/(main)/blog/page.tsx
@@ -2,32 +2,16 @@
 
 import { Suspense } from "react";
 import BrandsSection from "@/components/brands-section";
-import SpaceBadge from "@/components/space-badge";
 import JoinSection from "@/components/join-section";
-import { Logo } from "@/components/brand";
 import Container from "@/components/container";
 import Blog from "@/components/Blog/Index";
 
 function BlogContent() {
   return (
     <>
-      <Container className="container space-y-5 py-20">
-        <section className="container gap-5 py-5 md:py-20">
-          <div className="space-y-5 flex-col flex justify-center items-center text-center sm:w-1/2 mx-auto">
-            <Logo className="scale-75" />
-            <SpaceBadge>CODE SPACE BLOG</SpaceBadge>
-            <h1 className="font-medium text-3xl md:text-5xl leading-snug">
-              The Code Space Blog
-            </h1>
-            <p className="text-xl subtitle max-w-xl">
-              Stay updated with the latest news, insights, stories, and updates from the heart of our community.
-            </p>
-          </div>
-        </section>
-
+      <Container className="space-y-5">
         <Blog />
       </Container>
-
       <BrandsSection />
       <JoinSection />
     </>
--- src/components/Blog/FeaturedBlogs.tsx
+++ src/components/Blog/FeaturedBlogs.tsx
@@ -2,6 +2,8 @@ import { BlogsAPIResponse } from "@/types";
 import { formatDate } from "@/lib/utils";
 import Link from "next/link";
 import { MailIcon } from "lucide-react";
+import React from "react";
+
 
 export default function FeaturedBlogs({
   blogs,
@@ -13,7 +15,7 @@ export default function FeaturedBlogs({
       <div className="flex justify-between items-center">
         <h3 className="text-black text-lg md:text-xl font-medium">Featured</h3>
         <Link href="#footer" className="flex items-center gap-2 text-primary hover:text-primary/80 transition-colors">
-          <span className="text-sm md:text-base font-bold">Subscribe to the Mono blog</span>
+          <span className="text-sm md:text-base font-bold">Subscribe to the Code Space blog</span>
           <MailIcon size={20} />
         </Link>
       </div>
@@ -37,9 +39,12 @@ export default function FeaturedBlogs({
                 </p>
                 <Link
                   href={`/blog/${blog.id}`}
-                  className="inline-flex items-center text-primary hover:text-primary/90 transition-colors"
+                  className="inline-flex items-center text-primary hover:text-primary/90 transition-colors font-bold"
                 >
-                  Read the story →
+                  Read the story
+                  <svg width={14} height={10} viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg" className="ml-2 rotate-180">
+                    <path d="M13 5.6C13.3314 5.6 13.6 5.33137 13.6 5C13.6 4.66863 13.3314 4.4 13 4.4V5.6ZM0.575736 4.57574C0.341421 4.81005 0.341421 5.18995 0.575736 5.42426L4.39411 9.24264C4.62843 9.47696 5.00833 9.47696 5.24264 9.24264C5.47696 9.00833 5.47696 8.62843 5.24264 8.39411L1.84853 5L5.24264 1.60589C5.47696 1.37157 5.47696 0.991674 5.24264 0.757359C5.00833 0.523045 4.62843 0.523045 4.39411 0.757359L0.575736 4.57574ZM13 4.4H1V5.6H13V4.4Z" fill="currentColor" />
+                  </svg>
                 </Link>
               </div>
             </div>
