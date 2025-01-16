95f06630a469b9b37e8d6ddd69d5dd9a88554f3b
preciouskayili <preciouskayili@gmail.com>
2025-01-16 23:45:44 +0100

    Add Vercel Blob Storage plugin to Payload CMS configuration

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,4 +1,4 @@
-// import { vercelBlobStorage } from "@payloadcms/storage-vercel-blob";
+import { vercelBlobStorage } from "@payloadcms/storage-vercel-blob";
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { payloadCloudPlugin } from "@payloadcms/payload-cloud";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
@@ -34,5 +34,14 @@ export default buildConfig({
     url: process.env.DATABASE_URI || "",
   }),
   sharp,
-  plugins: [payloadCloudPlugin()],
+  plugins: [
+    payloadCloudPlugin(),
+    vercelBlobStorage({
+      enabled: true,
+      collections: {
+        media: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN,
+    }),
+  ],
 });
