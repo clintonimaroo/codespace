0b3aeaf63089592d65b5bff71976c9eb7c1f8032
namecannotbeblankk <93707197+clintonimaroo@users.noreply.github.com>
2025-01-19 14:05:40 +0100

    change

--- src/components/join-section.tsx
+++ src/components/join-section.tsx
@@ -6,7 +6,7 @@ const JoinSection = () => {
   return (
     <Container className="container flex flex-col lg:flex-row items-center py-20 mt-7 mb-20 gap-x-20 gap-y-10">
       <div className="w-full md:w-80">
-        <ImageCard className="w-full shadow-[0_0_20px_0_rgba(34,34,34,0.05)]" />
+        <ImageCard src={"/images/isabella.jpg"} className="w-full shadow-[0_0_20px_0_rgba(34,34,34,0.05)]" />
       </div>
       <div className="space-y-5 flex flex-col max-w-xl">
         <h3 className="text-xl text-primary font-medium">JOIN CODE SPACE!!</h3>
