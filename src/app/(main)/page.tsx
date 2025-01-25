ea6dfbfa5358f93734c364cb93989724fe6d0cb1
preciouskayili <preciouskayili@gmail.com>
2025-01-25 01:07:34 +0100

    fix: update VideoPlayer source URL for improved playback

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -148,7 +148,7 @@ const Home = () => {
           className="relative w-full mt-8"
         >
           <VideoPlayer
-            src="https://dl181.dmate13.online/?file=M3R4SUNiN3JsOHJ6WWQ2a3NQS1Y5ZGlxVlZIOCtyZ0NqY1l6eUI1b05hNVByc1lKMHZlMGFKc2ZmUE5FNVlPbEdkSmQ4amJNWXUvYVlGZlpwcjRPY0QyTTU0Sjd2emJEOXJFd1dOMTVDMU9xdmYrc2d5Vmppd0t3TFA3QUJld1RQMUVxNWhKRmlqRzN6OVdDalVQWG9rU1M5aDNiUERRc21nbEZNT1dFdDVoTjJqVCtYTEhoeTRBTXZ5T0w2NWRNaDZISzUxYWhsZUptNFlvelRVMXVkczBPZ2Mra2o2bVk5d0VLbHNRWTJCND0%3D"
+            src="https://rr4---sn-a5mekn6d.googlevideo.com/videoplayback?expire=1737785132&ei=zCqUZ8WeB-X3sfIPsaacyAI&ip=146.70.174.68&id=o-AMWvKObukkl-4oHJsryPYHmTIGHWbtsIGvuoZDmiLifq&itag=18&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&met=1737763532%2C&mh=mG&mm=31%2C26&mn=sn-a5mekn6d%2Csn-o097znsr&ms=au%2Conr&mv=m&mvi=4&pl=24&rms=au%2Cau&initcwndbps=1030000&bui=AY2Et-MmpA0981n2fFRVQ8SvX4FBBeJRw-xm6Btau4SNPdKdlF0jU85Ai9bvPImxfg7JTc9EBGRo8lmM&vprv=1&svpuc=1&mime=video%2Fmp4&ns=WiJLInWIYgIUMNuYnVDGHrMQ&rqh=1&cnr=14&ratebypass=yes&dur=82.245&lmt=1734005549369517&mt=1737763008&fvip=4&lmw=1&fexp=51326932%2C51335594%2C51353498%2C51371294&c=TVHTML5&sefc=1&txp=6208224&n=dzIOux6NNlb2Xg&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cbui%2Cvprv%2Csvpuc%2Cmime%2Cns%2Crqh%2Ccnr%2Cratebypass%2Cdur%2Clmt&lsparams=met%2Cmh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Crms%2Cinitcwndbps&lsig=AGluJ3MwRgIhAP8AdSVLKHcShp5LDqwVdr9wgpcgmvELsaabafZqWMMTAiEAqb23Btd2fNJ_ZGYNeRU-3Kygq1lpDAv2_frGgDN88i0%3D&sig=AJfQdSswRQIgDeyEyxkwxEXG8Zh4I47jyr0ZiNZ3DRs6J-XP61DmKaoCIQDEjpr0iU0fYStJESyk5MoXakF4vj26uULmxVC5WZMQkQ%3D%3D"
             autoPlay
           />
         </motion.div>
